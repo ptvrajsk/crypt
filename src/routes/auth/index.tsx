50ce46736e6230bdab2d51eb71d5144721df9723
Prithivi Raj <ptvrajsk@gmail.com>
2022-08-26 19:19:51 +0800

    React Routing Setup

---
+++ src/routes/auth/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Component_Auth() {
+  return (
+    <div>
+      This is the auth component
+    </div>
+  );
+}
+
---
+++ src/routes/vault/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Component_Vault() {
+  return (
+    <div>
+      This is the Vault Component
+    </div>
+  );
+}
