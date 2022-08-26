9a98ecccb7226632ef6988039ba025eb9113a55e
Guganesh <guganesh99@gmail.com>
2022-08-27 00:13:41 +0800

    temporary save

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
     return (
       <div className="App">
         <header className="App-header">
+          <h1>Crypt</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
